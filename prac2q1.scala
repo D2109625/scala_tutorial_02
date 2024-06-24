fe2cf8cd73de2978e63475114820fbaf445ab844
Dinushika Gedarawattage <106918606+D2109625@users.noreply.github.com>
2024-06-24 09:14:06 -0700

    Update prac2q1.scala

--- prac2q1.scala
+++ prac2q1.scala
@@ -15,3 +15,29 @@ object que01{
         println(i*n);
     }       
 }
+
+
+
+
+
+/*Differences Between Java and Scala
+>> Syntax:
+
+Scala is more concise and often has less boilerplate code compared to Java.
+Java requires more explicit type declarations and boilerplate.
+
+>> Variable Declaration and Initialization:
+   
+In Scala, multiple variables can be declared and initialized in a single line (e.g., var i, j, k: Int = 2).
+In Java, each variable needs to be declared and initialized separately (e.g., int i, j, k; i = j = k = 2). 
+ 
+>> Type Inference:
+
+Scala has type inference, meaning it can often deduce the type of a variable from the context, reducing the need for explicit type declarations.
+Java requires explicit type declarations for variables.
+
+>>Immutable vs Mutable Variables:
+
+Scala distinguishes between mutable (var) and immutable (val) variables.
+Java uses final for immutability, but by default, variables are mutable */
+
